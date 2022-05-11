0a96b0545ce522e91b40154db7a241fd3fdc1e3a
JulCzar <julio.monteiro@tonolucro.com.br>
2022-05-10 22:11:20 -0300

    added route to cvc route again

--- src/screens/Game/views/PvC.tsx
+++ src/screens/Game/views/PvC.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useGameContext } from '../../../services';
 import { Board } from '../../../shared';
 import { Color } from '../../../types';
 
 const PvP: React.FC = () => {
   const { setAI } = useGameContext();
+  const navigate = useNavigate();
 
   useEffect(() => {
     setAI([Color.R]);
@@ -13,7 +15,19 @@ const PvP: React.FC = () => {
     return () => setAI([]);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  return <Board />;
+  return (
+    <>
+      <Board />
+
+      <button
+        onClick={() => {
+          navigate('/game/cvc');
+        }}>
+        {' '}
+        improve ai
+      </button>
+    </>
+  );
 };
 
 export default PvP;
