76cde9f202bc897f2bd3a477cf7082dacbafa2af
JulCzar <julio.monteiro@tonolucro.com.br>
2022-05-10 22:05:25 -0300

    added sigmoide

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,5 @@ import { Color, PlayerName } from '../types';
 export const colorToPlayerName = (color: Color): PlayerName => {
   return color === Color.R ? 'red' : 'yellow';
 };
+
+export const sigmoide = (x: number): number => 1 / (1 + Math.exp(-x));
